e44685fed50a2fccc14018c79421109e2dabee92
Josh Lim <sam@gmail.com>
2022-10-02 22:55:10 +0800

    Initial Commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = User::updateOrCreate(
+            ['zoomid' => $request->get('zoomid')],[
+                'name' => $request->name,
+                'occupation' => $request->occupation,
+                'prior_exp1' => $request->prior_exp1,
+                'prior_exp2' => $request->prior_exp2,
+                'prior_exp3' => $request->prior_exp3,
+                'interest1' => $request->interest1,
+                'interest2' => $request->interest2,
+                'interest3' => $request->interest3,
+                'email' => $request->email,
+                'mtgobj1' => $request->mtgobj1,
+                'mtgobj2' => $request->mtgobj2,
+                'mtgobj3' => $request->mtgobj3,
+            ]
+        );
+        $user->save();
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $user=User::where('zoomid', $id)->firstOrFail();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user=User::where('zoomid', $id)->firstOrFail()->delete();
+        return 'Deletion Successful';
+    }
+}
---
+++ database/migrations/2022_10_01_105346_create_user_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->string('zoomid')->index();
+            $table->string('name')->nullable();
+            $table->string('occupation')->nullable();
+            $table->string('prior_exp1')->nullable();
+            $table->string('prior_exp2')->nullable();
+            $table->string('prior_exp3')->nullable();
+            $table->string('interest1')->nullable();
+            $table->string('interest2')->nullable();
+            $table->string('interest3')->nullable();
+            $table->string('email')->nullable();
+            $table->string('mtgobj1');
+            $table->string('mtgobj2')->nullable();
+            $table->string('mtgobj3')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
